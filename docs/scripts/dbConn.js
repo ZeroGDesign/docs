dd90a647a1a8ce3b2a5bd6efd2d9855979292bea
ZeroGDesign <jpaassen1994@gmail.com>
2023-09-21 23:24:40 +0200

    Removal of JSON BOM database, converting to YAML

--- docs/scripts/dbConn.js
+++ docs/scripts/dbConn.js
@@ -1,7 +1,26 @@
-const databasePath = window.location.origin+'/database/bom.json';
+// const databasePath = window.location.origin+'/database/bom_test.yaml';
+
+// async function fetch_Data() {
+//     return fetch(databasePath)
+//         .then(respone => respone.json())
+//         .catch(error => console.log('error', error));
+// }
+
+const databasePath = window.location.origin + '/database/bom.yaml';
 
 async function fetch_Data() {
-    return fetch(databasePath)
-        .then(respone => respone.json())
-        .catch(error => console.log('error', error));
+    try {
+        const response = await fetch(databasePath);
+        
+        if (!response.ok) throw new Error('Network response was not ok');
+
+        const textData = await response.text();
+        return jsyaml.load(textData); // Parse YAML using js-yaml library
+        
+    } catch (error) {
+        console.error('There has been a problem with your fetch operation:', error);
+    }
 }
+
+// Calling function to test - Disabled for now
+// fetch_Data().then(data => console.log(data));
