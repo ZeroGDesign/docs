3ebcdb567045793f8a95a3ad23dc5ae3bf8b3af6
ZeroGDesign <jpaassen1994@gmail.com>
2023-10-27 02:46:02 +0200

    Cleanup

--- docs/scripts/klipper_corexy_solver.js
+++ docs/scripts/klipper_corexy_solver.js
@@ -1,118 +1,92 @@
 function storeData() {
-    var data = document.getElementById("inputData").value;
+    const data = document.getElementById('inputData').value;
 
-    console.log("Data:", data);  // Log the input data to console
+    // Commented out console logs for cleaner output
+    // console.log('Data:', data);
 
-    if (!data || !data.includes("[stepper_x]") || 
-        !data.includes("[stepper_y]") || 
-        !data.includes("step_pin") || 
-        !data.includes("dir_pin") || 
-        !data.includes("enable_pin")) {
-            
-        console.log("Validation failed");  // Log validation result to console
-        document.getElementById("nowWhatText").innerHTML = '<span style="color:white"><center>Either you need to input your stepper config or your config is incorrect!</center></span>';
-
+    // Validation: Check if all necessary stepper configurations are present
+    if (!data || !['[stepper_x]', '[stepper_y]', 'step_pin', 'dir_pin', 'enable_pin'].every(prop => data.includes(prop))) {
+        // console.log('Validation failed');
+        document.getElementById('nowWhatText').innerHTML = '<span style="color:white;text-align:center;">Either input your stepper config or ensure it is correct!</span>';
         return;
     }
 
-    console.log("Validation passed");  // Log validation result to console
+    // console.log('Validation passed');
 
-    var structuredData = {
-        stepper_x: {},
-        stepper_y: {}
-    };
+    const structuredData = { stepper_x: {}, stepper_y: {} };
 
-
-    var currentSection = null;
-    var lines = data.split("\n").filter(Boolean);
-    
-    lines.forEach(function(line) {
-        if (line.startsWith("[stepper_x]")) {
-            currentSection = "stepper_x";
-            return;
-        } else if (line.startsWith("[stepper_y]")) {
-            currentSection = "stepper_y";
-            return;
-        }
-
-        var parts = line.split(/:|=/).map(s => s.trim());
-        if (currentSection) {
-            structuredData[currentSection][parts[0]] = parts[1];
+    // Processing input data into structured data object
+    let currentSection = null;
+    data.split('\n').filter(Boolean).forEach(line => {
+        if (line.startsWith('[stepper_x]')) currentSection = 'stepper_x';
+        else if (line.startsWith('[stepper_y]')) currentSection = 'stepper_y';
+        else if (currentSection) {
+            const [key, value] = line.split(/:|=/).map(s => s.trim());
+            structuredData[currentSection][key] = value;
         }
     });
 
-    sessionStorage.setItem("userInput", JSON.stringify(structuredData));
+    // Storing structured data for future use
+    sessionStorage.setItem('userInput', JSON.stringify(structuredData));
     updateNowWhatText(structuredData);
-
     applyLogic();
 }
 
 function updateNowWhatText(data) {
-    var content = formatDataToString(data);
-    var formattedHTML = `
+    const formattedHTML = `
 <div class="language-lua highlighter-rouge">
     <div class="highlight">
-        <pre class="highlight"><code id="output">${content}</code></pre>
+        <pre class="highlight"><code id="output">${formatDataToString(data)}</code></pre>
     </div>
-</div>
-    `;
-    document.getElementById("nowWhatText").innerHTML = formattedHTML;
+</div>`;
+    document.getElementById('nowWhatText').innerHTML = formattedHTML;
 }
 
 function formatDataToString(data) {
-    var result = "";
-    for (let section in data) {
-        result += `[${section}]\n`;
-        for (let key in data[section]) {
-            result += `${key}: ${data[section][key]}\n`;
-        }
-        result += "\n"; // Add an empty line between sections
-    }
-    return result.trim(); // Remove any trailing newline
+    return Object.entries(data)
+        .map(([section, values]) => `[${section}]\n${Object.entries(values).map(([k, v]) => `${k}: ${v}`).join('\n')}\n`)
+        .join('\n').trim();
 }
 
 function applyLogic() {
-    // Retrieve the stored data
-    var data = JSON.parse(sessionStorage.getItem("userInput"));
+    const data = JSON.parse(sessionStorage.getItem('userInput'));
+    const selections = `${document.getElementById('stepperA').value}-${document.getElementById('stepperB').value}`;
 
-    // Get selected values from the drop-down lists
-    var xSelection = document.getElementById("stepperA").value;
-    var ySelection = document.getElementById("stepperB").value;
+    const nowWhatText = document.getElementById('nowWhatText');
 
-    if (xSelection === "right" && ySelection === "rear") {
-        document.getElementById("nowWhatText").innerHTML = '<span style="color:lightgreen"><center>Your config seems good!<br/>Issues? Contact us on Discord.</center></span>';
-        return;
-    } else if (xSelection === "rear" && ySelection === "right") {
-        data.stepper_y.dir_pin = toggleExclamation(data.stepper_y.dir_pin);
-    } else if (xSelection === "front" && ySelection === "left") {
-        data.stepper_x.dir_pin = toggleExclamation(data.stepper_x.dir_pin);
-    } else if (xSelection === "left" && ySelection === "front") {
-        data.stepper_x.dir_pin = toggleExclamation(data.stepper_x.dir_pin);
-        data.stepper_y.dir_pin = toggleExclamation(data.stepper_y.dir_pin);
-    } else if (
-        (xSelection === "right" && ySelection === "front") ||
-        (xSelection === "rear" && ySelection === "left") ||
-        (xSelection === "front" && ySelection === "right") ||
-        (xSelection === "left" && ySelection === "rear")
-    ) {
-        document.getElementById("nowWhatText").innerHTML = '<span style="color:yellow"><center>Your steppers are swapped.<br/>You need to swap the A & B connectors, otherwise known as X & Y.</center></span>';
-        return;
-    } else {
-        document.getElementById("nowWhatText").innerHTML = '<span style="color:yellow"><center>The homing direciton issues might be related to something else.<br/>Ask help on the Discord server.</center></span>';
-        return;
-    }    
+    switch (selections) {
+        case 'right-front':
+        case 'rear-left':
+        case 'front-right':
+        case 'left-rear':
+            nowWhatText.innerHTML = createResponse('yellow', 'Your steppers are swapped.<br/>You need to swap the A & B connectors, otherwise known as X & Y.');
+            return;
+        case 'right-rear':
+            nowWhatText.innerHTML = createResponse('lightgreen', 'Your config seems good!<br/>Issues? Contact us on Discord.');
+            return;
+        case 'rear-right':
+            data.stepper_y.dir_pin = toggleExclamation(data.stepper_y.dir_pin);
+            break;
+        case 'front-left':
+            data.stepper_x.dir_pin = toggleExclamation(data.stepper_x.dir_pin);
+            break;
+        case 'left-front':
+            Object.keys(data).forEach(k => data[k].dir_pin = toggleExclamation(data[k].dir_pin));
+            break;
+        default:
+            nowWhatText.innerHTML = createResponse('yellow', 'The homing direction issues might be related to something else.<br/>Ask help on the Discord server.');
+            return;
+    }
 
-    // Update the "nowWhatText" section with the modified data
     updateNowWhatText(data);
 }
 
-// Helper function to toggle the "!" in dir_pin
 function toggleExclamation(value) {
-    if (value.startsWith('!')) {
-        return value.substring(1);
-    } else {
-        return '!' + value;
-    }
+    return value.startsWith('!') ? value.substring(1) : '!' + value;
+}
+
+function createResponse(color, message) {
+    return `<span style="color:${color};text-align:center;">${message}</span>`;
 }
 
 function onSaveButtonClick() {
